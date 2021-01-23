801e8ec657f4f9965e90a7e98794c1c0771b5f1e
YottaDon <dominik.strozynski96@gmail.com>
2021-01-23 21:58:21 +0100

    Made todolist as Electron app now

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    // resizable: false,
+    minWidth: 800,
+    minHeight: 600,
+    width: 1000,
+    hright: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("home.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
